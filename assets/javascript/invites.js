dafa421f00834d5c67232500be973ed9256b2d61
chesteraustin <chesteraustin@gmail.com>
2017-06-05 19:12:49 -0700

    have users saved to same node

--- assets/javascript/invites.js
+++ assets/javascript/invites.js
@@ -1,19 +1,63 @@
 /*Get list of people that are in this invite*/ 
-var database = firebase.database();
-var invites = database.ref("/invites");
-var invitesRef = database.ref("/invites").push();
+/*Check to see if there is an eventID already for this event */
+var eventID = localStorage.getItem("eventID")
+if (eventID != null) {
+	var database = firebase.database();
+	var invites = database.ref("/invites/");
+	var invitesRef = database.ref("/invites/" + eventID);
+	console.log("has already eventID = " + eventID);
+}
+else {
+	var database = firebase.database();
+	var invites = database.ref("/invites/");
+	var invitesRef = database.ref("/invites/").push();
+	var eventID = invitesRef.getKey();
+	localStorage.setItem('eventID', eventID);
+	console.log("has new eventID = " + eventID);
+}
+var peopleInvited = [];
 
-//Read Database
-invites.on("child_added", function(peopleRef) {
+//Initial load of firebase data
+invitesRef.once("value", function(peopleRef) {
 	peopleRef.forEach(function(personRef){
 		var person = personRef.val();
-		var key = personRef.getKey();
-		var inviteName = person.name;
+		var invitee = {};
+		invitee.inviteName = person.name;
+		invitee.isAvailable = person.isAvailable;
+		invitee.key = personRef.getKey();
+		invitee.longitude  = person.lng;
+		invitee.latitude  = person.lat;
+		peopleInvited.push(invitee)
+
+		//Create DIV to hold data
+		var person_div = $("<div>");
+		person_div.html(invitee.inviteName + " | " + invitee.isAvailable + " ["+ invitee.key + "]" + "( " + invitee.longitude + " , " + invitee.latitude + " )");
+		$("#inviteList").append(person_div);
+	})
+})
+
+//Read Database
+invites.on("child_changed", function(peopleRef) {
+	//empty inviteList div
+	$("#inviteList").empty();
+
+	peopleRef.forEach(function(personRef){
+		var person = personRef.val();
+		var invitee = {};
+		invitee.inviteName = person.name;
+		invitee.isAvailable = person.isAvailable;
+		invitee.key = personRef.getKey();
+		invitee.longitude  = person.lng;
+		invitee.latitude  = person.lat;
+		peopleInvited.push(invitee)
 
 		//Create DIV to hold data
 		var person_div = $("<div>");
 		person_div.html(inviteName);
 		$("#inviteList").append(person_div);
+
+		//put markers on map
+//		showMap()
 	})
 })
 
@@ -29,6 +73,7 @@ $(document).ready(function(){
 			"name": invitee,
 			"inviteKey": invitesRef.getKey()
 		});
+
 		//Clear input to add another invite
 		$("#add-invite").val('');
 	});
